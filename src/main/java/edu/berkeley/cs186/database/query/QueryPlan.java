67d90aec178474559936e7f6624c3c6f6d0b0eb3
Xi Gao <102536580+nivag8899@users.noreply.github.com>
2024-03-13 14:03:24 -0700

    Part 2 Task 5 finished.

--- src/main/java/edu/berkeley/cs186/database/query/QueryPlan.java
+++ src/main/java/edu/berkeley/cs186/database/query/QueryPlan.java
@@ -577,6 +577,19 @@ public class QueryPlan {
         QueryOperator minOp = new SequentialScanOperator(this.transaction, table);
 
         // TODO(proj3_part2): implement
+        int minCost = minOp.estimateIOCost();
+        int bestIndexColumn = -1;
+        for (int indexColumn : getEligibleIndexColumns(table)) {
+            SelectPredicate sp = this.selectPredicates.get(indexColumn);
+            QueryOperator indexScan = new IndexScanOperator(this.transaction, table, sp.column, sp.operator, sp.value);
+            int cost = indexScan.estimateIOCost();
+            if (cost < minCost) {
+                minCost = cost;
+                minOp = indexScan;
+                bestIndexColumn = indexColumn;
+            }
+        }
+        minOp = addEligibleSelections(minOp, bestIndexColumn);
         return minOp;
     }
 
