a0011606535e7d8d5defca435111fc2cbfb49ec8
Xi Gao <102536580+nivag8899@users.noreply.github.com>
2024-04-11 17:41:27 -0700

    LockUtil finished.

--- src/main/java/edu/berkeley/cs186/database/concurrency/LockUtil.java
+++ src/main/java/edu/berkeley/cs186/database/concurrency/LockUtil.java
@@ -2,48 +2,72 @@ package edu.berkeley.cs186.database.concurrency;
 
 import edu.berkeley.cs186.database.TransactionContext;
 
-/**
- * LockUtil is a declarative layer which simplifies multigranularity lock
- * acquisition for the user (you, in the last task of Part 2). Generally
- * speaking, you should use LockUtil for lock acquisition instead of calling
- * LockContext methods directly.
- */
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class LockUtil {
-    /**
-     * Ensure that the current transaction can perform actions requiring
-     * `requestType` on `lockContext`.
-     *
-     * `requestType` is guaranteed to be one of: S, X, NL.
-     *
-     * This method should promote/escalate/acquire as needed, but should only
-     * grant the least permissive set of locks needed. We recommend that you
-     * think about what to do in each of the following cases:
-     * - The current lock type can effectively substitute the requested type
-     * - The current lock type is IX and the requested lock is S
-     * - The current lock type is an intent lock
-     * - None of the above: In this case, consider what values the explicit
-     *   lock type can be, and think about how ancestor looks will need to be
-     *   acquired or changed.
-     *
-     * You may find it useful to create a helper method that ensures you have
-     * the appropriate locks on all ancestors.
-     */
-    public static void ensureSufficientLockHeld(LockContext lockContext, LockType requestType) {
-        // requestType must be S, X, or NL
-        assert (requestType == LockType.S || requestType == LockType.X || requestType == LockType.NL);
 
-        // Do nothing if the transaction or lockContext is null
+    public static void ensureSufficientLockHeld(LockContext lockContext, LockType lockType) {
         TransactionContext transaction = TransactionContext.getTransaction();
-        if (transaction == null || lockContext == null) return;
+        if (transaction == null || lockType.equals(LockType.NL)) {
+            return;
+        }
 
-        // You may find these variables useful
-        LockContext parentContext = lockContext.parentContext();
-        LockType effectiveLockType = lockContext.getEffectiveLockType(transaction);
-        LockType explicitLockType = lockContext.getExplicitLockType(transaction);
+        if (LockType.substitutable(lockContext.getEffectiveLockType(transaction), lockType)) {
+            return;
+        }
 
-        // TODO(proj4_part2): implement
-        return;
+        acquireParentLocksIfNeeded(lockContext, transaction, lockType);
+        acquireOrPromoteLock(lockContext, transaction, lockType);
     }
 
-    // TODO(proj4_part2) add any helper methods you want
+    private static void acquireParentLocksIfNeeded(LockContext lockContext, TransactionContext transaction, LockType lockType) {
+        LockType requiredParentLockType = (lockType == LockType.S) ? LockType.IS : LockType.IX;
+        ArrayList<LockContext> parentContexts = new ArrayList<>();
+        LockContext parent = lockContext.parent;
+
+        while (parent != null) {
+            parentContexts.add(parent);
+            parent = parent.parentContext();
+        }
+
+        Collections.reverse(parentContexts);
+        for (LockContext parentIterator : parentContexts) {
+            LockType currentParentLockType = parentIterator.getExplicitLockType(transaction);
+            if (currentParentLockType.equals(LockType.NL)) {
+                parentIterator.acquire(transaction, requiredParentLockType);
+            } else if (!LockType.substitutable(currentParentLockType, requiredParentLockType)) {
+                parentIterator.promote(transaction, requiredParentLockType);
+            }
+        }
+    }
+
+
+    private static void acquireOrPromoteLock(LockContext lockContext, TransactionContext transaction, LockType lockType) {
+        LockType explicitLockType = lockContext.getExplicitLockType(transaction);
+
+        if (explicitLockType.equals(LockType.NL)) {
+            lockContext.acquire(transaction, lockType);
+            return;
+        }
+
+        if (lockType.equals(LockType.S)) {
+            if (explicitLockType.equals(LockType.IS)) {
+                lockContext.escalate(transaction);
+            } else {
+                lockContext.promote(transaction, LockType.SIX);
+            }
+        } else {
+            if (!explicitLockType.equals(LockType.X)) {
+                if (LockType.canBeParentLock(explicitLockType, lockType)) {
+                    lockContext.promote(transaction, lockType);
+                } else {
+                    lockContext.escalate(transaction);
+                    if (!lockContext.getExplicitLockType(transaction).equals(LockType.X)) {
+                        lockContext.promote(transaction, LockType.X);
+                    }
+                }
+            }
+        }
+    }
 }
