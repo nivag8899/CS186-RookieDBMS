178d7781582fcd239e654764574a1f0ee3a48d32
Xi Gao <102536580+nivag8899@users.noreply.github.com>
2024-04-11 19:34:18 -0700

    Task 5: Two-Phase Locking finished.

--- src/main/java/edu/berkeley/cs186/database/Database.java
+++ src/main/java/edu/berkeley/cs186/database/Database.java
@@ -931,6 +931,12 @@ public class Database implements AutoCloseable {
         public void close() {
             try {
                 // TODO(proj4_part2)
+                TransactionContext transaction = TransactionContext.getTransaction();
+                List<Lock> acquiredLocks = lockManager.getLocks(transaction);
+
+                for (int acquiredLocksIterator = acquiredLocks.size() - 1; acquiredLocksIterator >= 0; acquiredLocksIterator --) {
+                    LockContext.fromResourceName(lockManager, acquiredLocks.get(acquiredLocksIterator).name).release(transaction);
+                }
                 return;
             } catch (Exception e) {
                 // There's a chance an error message from your release phase
